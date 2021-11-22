a8a9e6f7c9863ba90f525c3282ea802d4860c740
kirillzekn <47919858+kirillzekn@users.noreply.github.com>
2021-11-22 13:44:30 +0500

    vm size has been changed to B2s

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -24,7 +24,7 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
   default_node_pool {
     name            = "agentpool"
     node_count      = var.node_count
-    vm_size         = "Standard_B1ms"
+    vm_size         = "Standard_B2s"
   }
 
   service_principal {
